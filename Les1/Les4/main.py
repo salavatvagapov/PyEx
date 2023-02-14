6c443c963c09f497ff284408d7fb18d4949b84ae
salavatvagapov <vs777@mail.ru>
2023-02-14 22:19:23 +0300

    add tasks

---
+++ Les1/Les4/main.py
@@ -0,0 +1,28 @@
+# from random import randint
+# N = int(input("Введите количество чисел в первом списке: "))
+# M = int(input("Введите количество чисел в первом списке: "))
+# list_1 = list([randint(1, 100) for i in range(N)])
+# list_2 = list([randint(1, 100) for i in range(M)])
+# print (list_1)
+# print (list_2)
+
+# list_3 = []
+
+# for i in list_1:
+#     if i in list_2:
+#         list_3.append(i)
+      
+# print (list_3)
+
+
+
+# N = int(input("Введите количество кустов: "))
+# a = list(map(int, input("Введите количство ягод на каждом кусте: ").split()))
+
+# max = 0
+
+# for i in range (1,N-1):
+#     if (a[i]+a[i+1]+a[i-1] > max):
+#         max = a[i]+a[i+1]+a[i-1]
+
+# print("Максимальная урочжайность трех ближайших кустов -", max)
