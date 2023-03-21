b7c77b833b6755de8967e428300f482d3b930b57
salavatvagapov <vs777@mail.ru>
2023-03-21 19:00:57 +0300

    add Les10

---
+++ Les1/Les10/main.py
@@ -0,0 +1,15 @@
+# В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. 
+# Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без get_dummies?
+
+import pandas as pd
+import random
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame(lst ,columns={'WhoAmI'})
+data.head()
+
+
+data = pd.DataFrame(random.sample(['robot', 'human']*10, 10) ,columns={'WhoAmI'})
+
+print (data)
